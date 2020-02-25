d2bacd1c5cb1e36268cf8d806d76c93eb9fb4e8b
GermainGit <germain.prevot@gmail.com>
2020-02-25 14:55:47 +0100

    WIP front

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,15 @@
-$(document).ready(function() {
-    $(".hamburger").click(function() {
-        $(this).toggleClass("is-active");
-    });
+//$(this).toggleClass("is-active");
+window.addEventListener("DOMContentLoaded", (event) => {
+    burger = document.querySelector('.hamburger');
+    menu = document.querySelector('#menu');
+    title = document.querySelector('#menu .title');
+    nav = document.querySelector('#menu .nav-menu');
 
-
-});
-
+    burger.addEventListener("click", function(){
+        menu.classList.toggle("burger-click");
+        title.classList.toggle('burger-click');
+        burger.classList.toggle("is-active");
+        nav.classList.toggle("burger-click");
+    })
+})
 

01564b79cfa78eeee38ab7df8dbab74ad2801611
kchamiot <chamiot.k@gmail.com>
2020-03-28 15:17:02 +0100

    Add hula hop address in .env and refactor

--- src/Controller/BorrowController.php
+++ src/Controller/BorrowController.php
@@ -5,10 +5,7 @@ namespace App\Controller;
 
 
 use App\Entity\Borrow;
-use App\Entity\User;
-use App\Repository\BorrowRepository;
-use App\Repository\GameRepository;
-use App\Service\MailerManager;
+use App\Services\MailerManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,23 +14,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class BorrowController
 {
     /**
-     * @Route("/borrows/returnGame/{id}", name="return_game")
+     * @Route("/borrows/return/{id}", name="return_game")
      * @param MailerManager $mailerManager
-     * @param BorrowRepository $borrowRepository
-     * @param $id
      * @param Borrow $borrow
      * @param EntityManagerInterface $entityManager
      * @return RedirectResponse
      */
-    public function sendMail(MailerManager $mailerManager, BorrowRepository $borrowRepository, $id, Borrow $borrow, EntityManagerInterface $entityManager)
+    public function return(
+        MailerManager $mailerManager,
+        Borrow $borrow,
+        EntityManagerInterface $entityManager
+    )
     {
         $borrow->setIsReturned(true);
         $entityManager->persist($borrow);
         $entityManager->flush();
-
-        $borrow = $borrowRepository->find($id);
-        $mailerManager->createMail($borrow);
-
+        $mailerManager->sendReturnGameMail($borrow);
         return new RedirectResponse("/");
     }
 }
--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,39 +5,31 @@ namespace App\Controller;
 
 
 use App\Form\ContactFormType;
+use App\Services\MailerManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Address;
-use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 
 class ContactController extends AbstractController
 {
     /**
      * @Route("/contact", name="contact_page")
      * @param Request $request
-     * @param MailerInterface $mailer
+     * @param MailerManager $mailerManager
      * @return \Symfony\Component\HttpFoundation\Response
-     * @throws \Symfony\Component\Mailer\Exception\TransportExceptionInterface
      */
-    public function contact(Request $request, MailerInterface $mailer): Response
+    public function contact(Request $request, MailerManager $mailerManager): Response
     {
         $form = $this->createForm(ContactFormType::class);
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()){
-            $email = (new Email())
-                ->from(new Address($form->get("email")->getData()))
-                ->to(new Address("thiebaultbebert@gmail.com"))
-                ->subject($form->get("subject")->getData())
-                ->text($form->get("message")->getData());
-
-            $mailer->send($email);
+            $email = $form->get("email")->getData();
+            $subject = $form->get("subject")->getData();
+            $message = $form->get("message")->getData();
+            $mailerManager->sendContactMail($email,$subject,$message);
         }
-
 
        return $this->render("contact/index.html.twig", [
             "contactForm" => $form->createView()
